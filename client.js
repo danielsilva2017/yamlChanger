995ea55cbb65ffe22f81e57c7d6f8ccd5876397e
Daniel Silva <silvadaniel@ua.pt>
2020-02-08 18:09:40 +0000

    kubernetes files can be changed now

--- client.js
+++ client.js
@@ -12,15 +12,20 @@ namespace: 'default'
     a=JSON.stringify(pods);
     fs.writeFile("results/pods.json", JSON.stringify(pods, null, 4));
 });
+*/
 
-client.endpoints.get(function (err, endpoints) {
+/*
+client.endpoints.get("aaaj",function (err, endpoints) {
     if (!err) {
-      console.log('endpoints: ' + JSON.stringify(endpoints));
+        console.log("supp")
       b=JSON.stringify(endpoints)
       fs.writeFile("results/endpoints.json", JSON.stringify(endpoints, null, 4));
     }
+    else{
+        console.log(err)
+    }
 });
-
+/*
 client.services.get(function (err, data){
     console.log('endpoints: ' + JSON.stringify(data));
     fs.writeFile("results/services.json", JSON.stringify(data, null, 4));
@@ -40,10 +45,21 @@ reqOptions: {},
 namespace: 'default'
 });
 
-client2.deployments = client2.createCollection('deployments',null,null,{ apiPrefix : 'apis' });
+client2.deployments = client2.createCollection('deployments',null,null,{ apiPrefix : 'apis',namespaced: true});
 
 client2.deployments.get( function (err, data) {
     console.log("here"+JSON.stringify(data))
-    fs.writeFile("results/deployments.json", JSON.stringify(data, null, 4));
-    
+    var a = data[0].items.filter(a=>a.metadata.name=="tomcat-deployment")
+    var dataJson=a[0]
+    dataJson.spec.replicas=5;
+    console.log(dataJson)
+    client2.deployments.update('tomcat-deployment',dataJson,function (err, data) {
+        if(!err){
+            console.log("done")
+        }
+        else{
+            console.log("ups"+JSON.stringify(err))
+        }
+    });
 });
+
